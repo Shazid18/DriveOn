1bd27e8b81825118b1b1c81fe644b71fa4c1d38b
Ahmed Shamir Shazid <exvalo69@gmail.com>
2022-12-15 03:11:16 +0600

    Final commit

---
+++ demo/views.py
@@ -0,0 +1,289 @@
+from django.contrib.auth.models import User
+from django.contrib.auth.views import PasswordChangeView
+from django.shortcuts import render,HttpResponse,redirect
+from django.contrib.auth.forms import UserCreationForm,UserChangeForm,PasswordChangeForm
+from django.urls import reverse_lazy
+
+from carInfo.models import Rental_Car, Selling_Car, Driver
+from cusProfile.models import CusProfile
+from payment.models import *
+from rentInfo.models import Rent_Car_Record
+from .form import CreateUserFrom, PasswordChangingForm, UserUpdateForm, ProfileUpdateForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, logout
+from django.contrib.auth import login as auth_login
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+
+#idex page load korbe
+def index(request):
+    return render(request, 'index.html')
+
+
+#Login kora thakle home page load korbe
+# if we need to restict any page with login then we need to write the 12 num line code before that pages method line
+@login_required(login_url='login')
+def home(request):
+    return render(request, 'home.html')
+
+
+#sign-up er kaj korsi
+def registration(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        form = CreateUserFrom()
+
+        if request.method == 'POST': #form theke data netese
+            form = CreateUserFrom(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request, 'Account was created for '+ user)
+                return redirect('login')
+
+        context = {'form': form}
+        return render(request, 'registration.html', context)
+
+#login page er kaj korsi
+def login(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                auth_login(request, user)
+                return redirect('home')
+            else:
+                messages.info(request,'Username or Password is incorrect')
+
+
+        context = {}
+        return render(request, 'login.html', context) #login hobe
+
+
+#logout er kaj korsi
+def logoutUser(request):
+    logout(request)
+    return redirect('index')
+
+
+#password Update er kaj korsi
+class PasswordsChangeView(PasswordChangeView):
+
+    form_class = PasswordChangingForm
+
+    success_url = reverse_lazy('view-profile')
+
+
+# dorkar nai ei page.ei page er kaj holo password change korle ekta success msg dey
+# def password_Success(request):
+#     return render(request,'password_success.html', {})
+
+
+
+
+
+#rent car gula display er kaj korsi
+@login_required(login_url='login')
+def rent_car(request):
+
+    rentCar = Rental_Car.objects.all()
+
+    return render(request, 'rentcar.html', {'Rental_Cars': rentCar})
+
+
+
+#car rent neyar jonno je je information lage oi page er kaj eikhane
+@login_required(login_url='login')
+def booking_details(request,pk,id):
+
+    rentcars = Rental_Car.objects.filter(pk=pk).first()
+    userid = User.objects.filter(id=id).first()
+    contex = {
+        'rentcars': rentcars,
+    }
+
+
+    if (request.method == 'POST'):
+
+        rent_time = request.POST['rent_time']
+        return_time = request.POST['return_time']
+        pickup_loc = request.POST['pickup_loc']
+
+
+        rentInfo = Rent_Car_Record(rent_time=rent_time, return_time=return_time, pickup_loc=pickup_loc, rent_car_id=rentcars, user_profile = userid)
+
+
+        rentInfo.save()
+        return redirect('/rent-payment-confirmation')
+
+    return render(request, 'booking_details.html', contex)
+
+
+
+#payment er transition id neyar kaj korsi
+@login_required(login_url='login')
+def rentPaymentconformation(request):
+
+    rent_record = Rent_Car_Record.objects.all().last()
+    rentcars = rent_record.rent_car_id.id
+    userid = request.user.id
+    car_image = rent_record.rent_car_id.car_image
+    car_model = rent_record.rent_car_id.car_model
+
+
+
+    contex = {
+        'rent_record': rent_record,
+        'car_image': car_image,
+        'car_model': car_model
+    }
+
+    if (request.method == 'POST'):
+
+        transition_id = request.POST['transition_id']
+
+
+        rentPayment = Rental_Car_payment(rental_car_id=rentcars, user_profile_id=userid, transition_id=transition_id)
+
+
+        rentPayment.save()
+        return redirect('/home')
+
+
+    return render(request, 'rent-car-payment-confarmation.html', contex )
+
+
+
+
+#sell er gari gula display korar kaj korsi
+@login_required(login_url='login')
+def Buy_car(request):
+
+    sellCar = Selling_Car.objects.all()
+
+    return render(request, 'buy-car.html',{'Selling_cars': sellCar})
+
+
+
+
+#sell car gular details display korar kaj korsi
+@login_required(login_url='login')
+def car_details(request,pk):
+
+    cars = Selling_Car.objects.filter(pk=pk).first()
+    contex= {
+        'cars':cars
+    }
+
+    return render(request, 'car-details.html',contex)
+
+
+
+#sell car er order details & transition id nibo
+@login_required(login_url='login')
+def order_details(request,pk,id):
+    buycars = Selling_Car.objects.filter(pk=pk).first()
+    userid = User.objects.filter(id=id).first()
+    contex = {
+        'buycars': buycars,
+    }
+
+    if (request.method == 'POST'):
+        transition_id = request.POST['transition_id']
+
+        sellPayment = Sell_Car_Payment(sell_car_id=buycars, user_profile=userid, transition_id=transition_id)
+
+        sellPayment.save()
+        return redirect('/home')
+
+
+
+    return render(request, 'order-details.html',contex)
+
+
+
+
+#driver display er kaj korsi
+@login_required(login_url='login')
+def driver(request):
+
+    hireDriver = Driver.objects.all()
+
+    return render(request, 'driver.html',{'Hire_Drivers': hireDriver})
+
+
+
+
+#driver Payment er kaj korbo
+@login_required(login_url='login')
+def driver_payment(request,pk,id):
+    drivers= Driver.objects.filter(pk=pk).first()
+    userid = User.objects.filter(id=id).first()
+    contex = {
+        'drivers': drivers,
+        'userid': userid
+    }
+
+    if (request.method == 'POST'):
+        transition_id = request.POST['transition_id']
+
+        driiverPayment = Driver_Payment(driver_id=drivers, user_profile=userid, transition_id=transition_id)
+
+        driiverPayment.save()
+        return redirect('/home')
+
+    return render(request, 'driver-payment.html',contex)
+
+
+
+#profile update er kaj korbo
+@login_required(login_url='login')
+def editProfile(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST,
+                                   request.FILES,
+                                   instance=request.user.cusprofile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your account has been updated!')
+            return redirect('view-profile')
+
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.cusprofile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+
+    return render(request, 'edit-profile.html', context)
+
+
+
+#profile information display er kaj korsi
+@login_required(login_url='login')
+def viewProfile(request):
+
+    user_id = request.user
+    pro_info = CusProfile.objects.filter(user=request.user).first()
+
+    context={
+        'user_id': user_id,
+        'pro_info': pro_info
+
+    }
+
+
+    return render(request, 'view-profile.html', context)
---
+++ pythonProject/urls.py
@@ -0,0 +1,47 @@
+"""pythonProject URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path
+from django.contrib.auth import views as auth_views
+import demo.views as driveon
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', driveon.index, name='index'),
+    path('home/', driveon.home, name='home'),
+    path('registration/', driveon.registration, name='registration'),
+    path('login/', driveon.login, name='login'),
+    path('logout/', driveon.logoutUser, name='logout'),
+    path('change-password/', driveon.PasswordsChangeView.as_view(template_name='change-password.html'), name='change-password'),
+    # path('password_success/', driveon.password_Success, name='password_success'),
+    path('rent-car/', driveon.rent_car, name='rent-car'),
+    path('booking-details/<int:pk>/<str:id>', driveon.booking_details, name='booking-details'),
+    path('rent-payment-confirmation/', driveon.rentPaymentconformation, name='rent-payment-confirmation'),
+    path('buy-car/', driveon.Buy_car, name='buy-car'),
+    path('car-details/<int:pk>', driveon.car_details, name='car-details'),
+    path('sell-payment-Confirmation/<int:pk>/<str:id>', driveon.order_details, name='sell-payment-Confirmation'),
+    path('driver/', driveon.driver, name='driver'),
+    path('driver-payment/<int:pk>/<str:id>', driveon.driver_payment, name='driver-payment'),
+    path('profile/', driveon.viewProfile, name='view-profile'),
+    path('edit-profile/', driveon.editProfile, name='edit-profile'),
+    path("reset_password/", auth_views.PasswordResetView.as_view(),name='password_reset'),  #1. Submit Email From
+    path("reset_password_sent/", auth_views.PasswordResetDoneView.as_view(),name='password_reset_done'),  #2. Email sent success message
+    path("reset/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(),name='password_reset_confirm'),  #3. Link to password reset from in email
+    path("reset_password_complete/", auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'),name='password_reset_complete'),  #4. Password successfully changed message
+
+]+static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
